b51a04b6717ef117635720a85847a8fe43e91362
Nizami Amirov <amirovnizami395@gmail.com>
2025-02-22 00:00:26 +0400

    Removed duplicate folder or renamed it

--- ManagementSystem.Domain/Entities/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿using ManagementSystem.Domain.BaseEntities;
-
-namespace ManagementSystem.Domain.Entities;
-
-public class Category :BaseEntity
-{
-    public string Name { get; set; }
-}
--- ManagementSystem.Repository/Common/IUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-﻿using ManagementSystem.Repository.Repositories;
-
-namespace ManagementSystem.Repository.Common;
-
-public interface IUnitOfWork
-{
-    public ICategoryRepository CategoryRepository { get;}
-}
